fe1405475b2bf7e5a5322413c84740b2a30b7cdd
Grace Nduta <ndutak5674@gmail.com>
2025-03-13 15:52:58 +0500

    complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const assembledBatteries = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries=assembledBatteries.reduce((sum ,battery)=>sum + battery, 0)
+   
+module.exports={totalBatteries};
